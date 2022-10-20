984c34545e5f1a8520b22d04b0b7cdc57ed1d453
amaan562 <19B81A0562@cvr.ac.in>
2022-10-20 23:38:20 +0530

    completed bank details

--- src/components/BankDetails/BankDetails.js
+++ src/components/BankDetails/BankDetails.js
@@ -13,22 +13,55 @@ import { useState } from 'react';
 function BankDetails() {
 
   useEffect(()=>{
+      // (async ()=>{
+      //   await setUsername(localStorage.username);
+      //   var url = `http://localhost:8017/bankDetails/${username}`;
+      //   console.log(url+" hehe here it is")
+      //   var data = await Axios.get(url);
+      //   await setPan(data.pan);
+      //   await setACNumber(data.accountNumber);
+      //   await setIfsc(data.ifsc);
+      //   await setFirstEmployment(data.firstEmployment);
+      // })();
       setUsername(localStorage.username);
-      var url = `http://localhost:8017/bankDetails/${username}`;
+      var url = `http://localhost:8017/bankDetails/${localStorage.username}`;
       console.log(url+" hehe here it is")
       Axios.get(url).then((data)=>{
-        setPan(data.pan);
-        setACNumber(data.accountNumber);
-        setIfsc(data.ifsc);
-        setFirstEmployment(data.firstEmployment);
+        console.log(data)
+        setPan(data.data.pan);
+        setACNumber(data.data.accountNumber);
+        setIfsc(data.data.ifsc);
+        setFirstEmployment(data.data.firstEmployment);
+        if(pan!==null)  setNewUser(false);
+        console.log(newUser+" hehe first emp")
       })
   },[]) 
+  const [newUser,setNewUser] = useState(true);
   const [username,setUsername] = useState("");
   const [pan,setPan]=useState("")
   const [ACNumber,setACNumber]=useState("")
   const [ifsc, setIfsc] = useState("");
   const [firstEmployment, setFirstEmployment] = useState(false);
-  const handleSubmit=()=>{  
+  const handleSubmit=async (e)=>{  
+      e.preventDefault();
+      var details = {
+        username : username,
+        pan : pan,
+        ifsc : ifsc,
+        accountNumber : ACNumber,
+        firstEmployment : firstEmployment
+      }
+      var url = "http://localhost:8017/bankDetails";
+      if(!newUser){
+        url+="/"+username;
+        var res = await Axios.put(url,details);
+        console.log("put called");
+      }
+      else{
+        var res = await Axios.post(url,details);
+        console.log("post called");
+      }
+      console.log(res);
       alert("Form submitted !!!");
   }
   return (
@@ -68,7 +101,7 @@ function BankDetails() {
                       </Col>
                   </Form.Group>
                   <Form.Group as={Row} className="mb-3">
-                      <Button className="save"  type="submit" onClick={handleSubmit}>Submit</Button>
+                      <Button className="save"  type="submit" onClick={(e)=>handleSubmit(e)}>Submit</Button>
                   </Form.Group>
                 </Form>
           
