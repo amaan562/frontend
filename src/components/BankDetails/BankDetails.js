3407b40e33f876cd3478b50a4fec4fc591ee82cd
amaan562 <19B81A0562@cvr.ac.in>
2022-10-20 14:13:22 +0530

    a few changes

--- src/components/BankDetails/BankDetails.js
+++ src/components/BankDetails/BankDetails.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import Axios from 'axios'
 import Navbar from '../Navbar'
 import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
-//import BootstrapSwitchButton from 'bootstrap-switch-button-react'
+import BootstrapSwitchButton from 'bootstrap-switch-button-react'
 import Form from 'react-bootstrap/Form';
 import Card from 'react-bootstrap/Card'
 import './BankDetails.css'
@@ -11,6 +12,18 @@ import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 function BankDetails() {
 
+  useEffect(()=>{
+      setUsername(localStorage.username);
+      var url = `http://localhost:8017/bankDetails/${username}`;
+      console.log(url+" hehe here it is")
+      Axios.get(url).then((data)=>{
+        setPan(data.pan);
+        setACNumber(data.accountNumber);
+        setIfsc(data.ifsc);
+        setFirstEmployment(data.firstEmployment);
+      })
+  },[]) 
+  const [username,setUsername] = useState("");
   const [pan,setPan]=useState("")
   const [ACNumber,setACNumber]=useState("")
   const [ifsc, setIfsc] = useState("");
@@ -27,31 +40,31 @@ function BankDetails() {
                 <Form>
                   <Form.Group as={Row} className="mb-3" controlId="pan" required>
                     <Col md={4}><Form.Label>Permanent Account Number (PAN)</Form.Label></Col>
-                    <Col md={8}><Form.Control  required={true}   type="text" placeholder="Enter PAN" onChange={e=>{setPan(e.target.value)}} /></Col>
+                    <Col md={8}><Form.Control  required={true}   type="text" placeholder="Enter PAN" onChange={e=>{setPan(e.target.value)}} value={pan} /></Col>
                   </Form.Group>
 
                   <Form.Group as={Row} className="mb-3" controlId="acno" required>
                   <Col md={4}><Form.Label>Account Number</Form.Label></Col>
-                  <Col md={8}><Form.Control type="text" placeholder="Enter A/C Number" onChange={e=>{setACNumber(e.target.value)}}   /></Col>
+                  <Col md={8}><Form.Control type="text" placeholder="Enter A/C Number" onChange={e=>{setACNumber(e.target.value)}} value={ACNumber}  /></Col>
                   </Form.Group>
 
                   <Form.Group as={Row} className="mb-3" controlId="ifsc" required>
                   <Col md={4}><Form.Label>Branch IFSC Code</Form.Label></Col>
-                  <Col md={8}><Form.Control type="text" placeholder="Enter IFSC Code" onChange={e=>{setIfsc(e.target.value)}}   /></Col>
+                  <Col md={8}><Form.Control type="text" placeholder="Enter IFSC Code" onChange={e=>{setIfsc(e.target.value)}}  value={ifsc} /></Col>
                   </Form.Group>
 
                   <Form.Group as={Row} className="mb-3" controlId="firstEmployment">
                   <Col md={4}><Form.Label>Is this your first employment ?</Form.Label></Col>
                   <Col md={8}>
-                    {/* <BootstrapSwitchButton 
-                      checked={true}
+                    <BootstrapSwitchButton 
+                      checked={firstEmployment}
                       width={100} 
                       onlabel="Yes" 
                       offlabel="No" 
                       onChange={(checked) => {
                         setFirstEmployment(checked)
                     }}
-                      />  */}
+                      /> 
                       </Col>
                   </Form.Group>
                   <Form.Group as={Row} className="mb-3">
